b0efae17222b362ab522e7cfca76f2583c8d676e
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-04-18 17:48:50 +0200

    add API routes for places

--- src/pages/api/places/index.js
+++ src/pages/api/places/index.js
@@ -5,14 +5,15 @@ export default async function handler(request, response) {
   await dbConnect();
 
   switch (request.method) {
-    // case "GET":
-    //   const places = await Place.find();
-    //   if (!places) {
-    //     return response.status(404).json({ status: "Not found" });
-    //   }
-    //   return response.status(200).json(places);
+    case "GET":
+      const places = await Place.find();
+      if (!places) {
+        console.log("nothing here");
+        return response.status(404).json({ status: "Not found" });
+      }
+      return response.status(200).json(places);
 
-    //   break;
+      break;
     case "POST":
       try {
         const placeData = request.body;
