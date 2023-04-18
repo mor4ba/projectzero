48f9361d3bf62c99d57723ef16047b2c709b8de6
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-04-18 17:49:03 +0200

    update Place model

--- db/models/Place.js
+++ db/models/Place.js
@@ -4,11 +4,21 @@ const { Schema } = mongoose;
 
 const placeSchema = new Schema({
   name: { type: String, required: true },
+  //created: new Date(),
   location: { type: String, required: true },
   //image: { type: String, required: false },
   //mapURL: { type: String, required: true },
-  description: { type: String, required: false },
-  //comments: [],
+  comment: [{ type: String, required: true }],
+  r_dresscode: [{ type: Number, required: true }],
+  r_amountOfPeople: [{ type: Number, required: true }],
+  r_volume: [{ type: Number, required: true }],
+  r_isSmoking: [{ type: Number, required: true }],
+  r_temperature: [{ type: Number, required: true }],
+  r_lights: [{ type: Number, required: true }],
+  r_cringe: [{ type: Number, required: true }],
+  r_age: [{ type: Number, required: true }],
+  r_staff: [{ type: Number, required: true }],
+  r_prices: [{ type: Number, required: true }],
 });
 
 const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
