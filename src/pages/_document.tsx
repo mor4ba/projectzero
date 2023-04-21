13da4923bfe1adfc0ec013018bf33ada512cc248
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-04-21 14:46:25 +0200

    add global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Header from "../components/Header";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="bg-gradient-to-r from-sky-500 to-indigo-500">
+        <Header />
         <Main />
         <NextScript />
       </body>
