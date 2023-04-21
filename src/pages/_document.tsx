22d683c34090c238b182b289cd59e0c93bf8d3a6
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-04-21 13:23:39 +0200

    add global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-gradient-to-r from-sky-500 to-indigo-500">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
