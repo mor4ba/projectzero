a5546cd7f6b909916f2de1c3ffa52a67e5a3f9d7
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-04-26 17:44:37 +0200

    add Info/plus SVG + Components

---
+++ src/components/graphics/Plus.js
@@ -0,0 +1,15 @@
+export default function addPlace() {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="27"
+      height="27"
+      viewBox="0 0 24 24"
+      className="fill-emerald-500"
+    >
+      <title>plus</title>
+      <path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z"></path>
+    </svg>
+  );
+}
