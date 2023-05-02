8eef58e6f674751cc2c6cef03249c04998393320
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-05-02 12:11:46 +0200

    Add relational links to OffCanvas

--- src/components/Offcanvas.js
+++ src/components/Offcanvas.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 import Link from "next/link";
+import Login from "./Login";
+import { useSession } from "next-auth/react";
 
 export default function Offcanvas({ offCanvasState, handleOffCanvasToggle }) {
+  const { data: session } = useSession();
+  console.log(session);
+
+  const userIsLoggedIn = (
+    <Link
+      className="cursor-pointer bg-black p-2"
+      href="/places/bucket"
+      onClick={() => handleOffCanvasToggle(false)}
+    >
+      bucket list
+    </Link>
+  );
   return (
     <div
       className={`offcanvas offcanvas__container flex flex-col justify-between fixed opacity-70 top-0 right-0 z-20 bg-white h-full w-2/5 transition-transform duration-300 p-10 border-l-4 border-sky-500 ${
@@ -23,17 +37,10 @@ export default function Offcanvas({ offCanvasState, handleOffCanvasToggle }) {
         >
           places
         </Link>
+        {session ? userIsLoggedIn : null}
       </nav>
 
-      <button
-        type="button"
-        className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-        onClick={() => push("/places")}
-      >
-        <span className="relative px-5 py-2 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          log in
-        </span>
-      </button>
+      <Login session={session} />
     </div>
   );
 }
