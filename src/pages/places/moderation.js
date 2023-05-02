95b6acd4caf048c053be3577cf6edc2ca8256032
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-05-02 17:23:23 +0200

    Set DataArray to filtered for InModeration in map/moderation.js

---
+++ src/pages/places/moderation.js
@@ -0,0 +1,43 @@
+import useSWR from "swr";
+import List from "../../components/List";
+
+export default function Moderation() {
+  const places = useSWR("/api/places", {});
+  const { data, isLoading } = places;
+
+  async function validateEntry(id) {
+    const response = await fetch(`/api/places/`, {
+      method: "PATCH",
+      body: JSON.stringify(id),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      await response.json();
+      places.mutate();
+      //push("/");
+      // handleClose();
+    } else {
+      console.error(`"Error: ${response.status}`);
+    }
+  }
+
+  if (isLoading) return <h1>We gonna load this for a second!</h1>;
+
+  const filteredData = data.filter((place) =>
+    place.inModeration ? place : null
+  );
+
+  return (
+    <>
+      <h1>Places in Moderation</h1>
+      <List
+        data={filteredData}
+        moderation={true}
+        handleValidateEntry={validateEntry}
+      />
+    </>
+  );
+}
