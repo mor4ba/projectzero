ab457a987e66e236a573e8601576c65147f6368e
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-05-04 18:49:49 +0200

    change Graphic Components

---
+++ src/components/graphics/Arrow.js
@@ -0,0 +1,18 @@
+export default function Arrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 22 22"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6 -mt-1 text-secondary-darker"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+      />
+    </svg>
+  );
+}
