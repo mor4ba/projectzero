e43eed5d8cf628c6b9ca8ac8ce2ab9fab91463ed
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-05-11 17:36:33 +0200

    comment out unnessary things in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import { Inter } from "next/font/google";
 import RenderMap from "../components/Map.js";
-const inter = Inter({ subsets: ["latin"] });
-import InitOverlay from "../components/InitOverlay.js";
-import { useState } from "react";
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
+// import InitOverlay from "../components/InitOverlay.js";
+// import { useState } from "react";
 import useSWR from "swr";
 import Spinner from "@/components/Spinner.js";
 
 export default function Home() {
   const places = useSWR("/api/places", { fallbackData: [] });
-  const [isWelcomeScreen, setIsWelcomeScreen] = useState(true);
+  // const [isWelcomeScreen, setIsWelcomeScreen] = useState(true);
 
   if (places.isLoading) return <Spinner />;
 
