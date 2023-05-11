3ff9fadefef79302f382c5f0c4a1c7a6f56b75ae
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-05-11 17:11:37 +0200

    CSS changed

--- src/components/graphics/Filter.js
+++ src/components/graphics/Filter.js
@@ -6,7 +6,7 @@ export default function Filter() {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="w-8 h-8 text-darker-grey hover:text-secondary-color transition-all"
+      className="w-9 h-9 text-darker-grey hover:text-secondary-color transition-all top-2 relative"
     >
       <path
         strokeLinecap="round"
